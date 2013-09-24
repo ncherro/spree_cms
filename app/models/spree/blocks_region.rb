8ca3d57ac38710fa614f32f93069977613ea97ed
Nick Herro <ncherro@gmail.com>
2013-09-24 18:33:38 -0400

    defined the blocks

--- app/models/spree/blocks_region.rb
+++ app/models/spree/blocks_region.rb
@@ -3,6 +3,9 @@ class Spree::BlocksRegion < ActiveRecord::Base
   validates :spree_block_id, presence: true
 
   belongs_to :block, class_name: "Spree::Block", foreign_key: "spree_block_id"
+  belongs_to :menu_block, class_name: "Spree::Block", foreign_key: "spree_block_id"
+  belongs_to :html_block, class_name: "Spree::Block", foreign_key: "spree_block_id"
+  belongs_to :static_block, class_name: "Spree::Block", foreign_key: "spree_block_id"
   belongs_to :region, class_name: "Spree::Region", foreign_key: "spree_region_id"
 
   attr_accessible :position, :spree_block_id, :spree_region_id,
